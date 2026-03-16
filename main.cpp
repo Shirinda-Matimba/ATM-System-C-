ec9a034c23c26935cd9784839aefa459718015cc
matimba <touchmatimba9@gmail.com>
2026-03-16 15:16:10 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    cout << "This is me the guy you know" << endl;
+    cout << "The guy you know?:" << endl;
+    cout << "This is me?:" << endl;
+    return 0;
+}
